35d786a5c8163d06737d26d644ff731b802a67b5
ilara90 <68242308+ilara90@users.noreply.github.com>
2020-10-12 21:29:58 +0300

    init: function countCats and createDreamTeam

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let matrixNew = matrix.reduce(
+      (result, element) => result.concat(element),
+      []
+    );
+  let result = 0;
+  for (i = 0; i < matrixNew.length; i++) {
+    if (matrixNew[i] === '^^') result = result + 1;
+  }
+  return result;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false
+  let arrayСapitalLetter = '';
+  for (var i = 0; i < members.length; i++) {
+    let element = members[i];
+    if (typeof(element) === "string") {
+      arrayСapitalLetter += element.trim()[0].toUpperCase();
+    }
+  }
+  let result = arrayСapitalLetter.split('');
+  let resultSort = result.sort();
+  return resultSort.join('');
 };
