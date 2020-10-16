288e1382f52d2a5e70e3a3553bc9053861b9e463
ilara90 <68242308+ilara90@users.noreply.github.com>
2020-10-16 21:27:52 +0300

    fix: Fixed some bugs in functions DepthCalculator,  transform.

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,21 @@
 const CustomError = require("../extensions/custom-error");
-
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+  calculateDepth(arr) {
+    if (!Array.isArray(arr)) return 0;
+    var ArrDepth = function (arr, depth) { 
+      var depthArray = [];
+      for (var i = 0; i < arr.length; i++) {
+          if (Array.isArray(arr[i])) {
+            depth++;
+            depthArray.push(ArrDepth(arr[i], depth));
+          }
+        } return depthArray.length === 0 ? depth : Math.max.apply(null, depthArray)
+    }
+    var depth = [];
+    for (var i = 0; i < arr.length; i++) {
+  if (Array.isArray(arr[i])) {
+    depth.push(ArrDepth(arr[i], 2))
+  } 
+} return depth.length === 0 ? 1 : Math.max.apply(null, depth);
+}
+}
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,27 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(arr) {
-  var a = arr.length
-  if (!Array.isArray(arr)) throw new Error();
+module.exports = function transform(arrFirst) {
+  var a = arrFirst.length;
+  if (!Array.isArray(arrFirst)) throw new Error();
+  var arr = [...arrFirst];
   if( arr[0] === '--double-prev' || arr[0] === '--discard-prev') {
     arr.splice(0, 1);
-  } else if(arr[a-1] === '--double-next' || arr[a-1] === '--discard-next') {
+  }
+   if(arr[a-1] === '--double-next' || arr[a-1] === '--discard-next') {
     arr.splice(a-1, 1);
-  } else {
-    for (var i = 0; i < arr.length; i++) {
-      if ( arr[i] === '--double-next') {
+  }
+    for (var i = 0; i < a; i++) {
+      if ( arr[i] === '--double-next' && i !==  a - 1) {
         arr[i] = arr[i+1];
       } 
-      if ( arr[i] === '--double-prev') {
+      if ( arr[i] === '--double-prev' && i !== 0) {
         arr[i] = arr[i-1];
       }
-      if ( arr[i] === '--discard-next') {
+      if ( arr[i] === '--discard-next' && i !==  a - 1) {
+        if (arr.length > i+3 && typeof(arr[i+2]) != "number") {
+          arr.splice(i+2, 1);
+        }
        arr.splice(i, 2);
       } 
-      if ( arr[i] === '--discard-prev') {
+      if ( arr[i] === '--discard-prev' && i !== 0) {
        arr.splice(i-1, 2);
       }
     }
-  }
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === '--double-next' || arr[i] === '--double-prev' || arr[i] === '--discard-next' || arr[i] === '--discard-prev') {
+        transform(arr)
+      }
+    }
   return arr;
 };
