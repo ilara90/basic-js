08c34628a68e8a5e202dddba321b1e6c0f204ff6
ilara90 <68242308+ilara90@users.noreply.github.com>
2020-11-01 21:20:03 +0300

    fix: many bag in function VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  
+  encrypt (str, key, mode) {
+  var output = [str.length];
+  var result = 0;
+  var output_str;
+
+  for (var i = 0; i < str.length; i++) {
+    if (mode == 1) {
+      result = ((str.charCodeAt(i) + key.charCodeAt(i % key.length)) % 128);
+      output[i] = String.fromCharCode(result);
+      console.log( 
+      str[i],key[i],result,output[i])
+
+    } else if (mode == 0) {
+      if (str.charCodeAt(i) - key.charCodeAt(i % key.length) < 0) {
+        result = (str.charCodeAt(i) - key.charCodeAt(i % key.length)) + 128;
+      } else {
+        result = (str.charCodeAt(i) - key.charCodeAt(i % key.length)) % 128;
+      }
+      output[i] = String.fromCharCode(result);
+    }
+
+  }
+  output_str = output.join('');
+  return output_str;
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(cipher, key) {
+    let lang = "en";
+    if (lang !== "ru" && lang !== "en" || cipher.length !== key.length) return false;
+    var s = "";
+    for (var i = 0; i < cipher.length; i++) {
+        var row = this[lang].indexOf(key[i])
+            coll = this.square[row].indexOf(cipher[i]);
+        s += this[lang][coll];
+    }
+    return s;
   }
 }
 
